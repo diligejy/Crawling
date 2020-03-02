690d1b599718d3bc0add09d0255a1ba3b17aefe7
jinyoung <sjy049@gmail.com>
2020-03-03 00:57:06 +0900

    기초 시작

--- janjaemi/1.py
+++ janjaemi/1.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 chromedriver = 'C:/Users/jinyoung/Pictures/Crawling/janjaemi/chromedriver'
 driver = webdriver.Chrome(chromedriver)
@@ -13,3 +14,30 @@ assert "Python" in driver.title
 # find_element_by_name('q')
 # find_elements_by_name('')
 elem = driver.find_element_by_name('q')
+
+# clear() : input 텍스트 초기화 하기
+# send_keys(키워드) : 키보드 입력값 전달하기
+#    - Keys.RETURN - 엔터키
+#    - dir(Keys)로 키에 대응되는 이름 찾기
+
+# input 텍스트 초기화
+elem.clear()
+
+# 키 이벤트 전송
+elem.send_keys('Python')
+
+# 엔터 입력
+elem.send_keys(Keys.RETURN)
+
+# assert로 driver.page_source에서 특정 키워드 확인하기
+# time.sleep()함수로 일정시간 브라우저 내용 확인할 수 있도록 하기
+# driver.quit()함수로 브라우저 끝내기
+
+assert "No results found." not in driver.page_source
+
+# 명시적으로 일정 시간 기다리기
+time.sleep(1)
+
+h3s = driver.find_elements_by_tag_name("h3")
+for h3 in h3s:
+    print(h3.text)
